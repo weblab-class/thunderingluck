49d8c1d1766a26284e48be22e16411d3d3d4cfb5
Eva Ge <evag@mit.edu>
2024-02-22 19:47:11 -0500

    removed +=googletag

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App.js";
 
-const googletag = `
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PLL1BLGSH9"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// const googletag = `
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-PLL1BLGSH9"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
 
-  gtag('config', 'G-PLL1BLGSH9');
-</script>
-`
+//   gtag('config', 'G-PLL1BLGSH9');
+// </script>
+// `
 
-document.getElementsByTagName("head")[0].innerHTML += googletag;
+// document.getElementsByTagName("head")[0].innerHTML += googletag;
 
 // renders React Component "Root" into the DOM element with ID "root"
 const container = document.getElementById("root");
